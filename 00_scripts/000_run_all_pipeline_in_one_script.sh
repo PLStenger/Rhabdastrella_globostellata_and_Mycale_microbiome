6e43f876b70455f06cf593a3c28cdd6d041f0d6a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-10-12 14:53:37 +1100

    Update 000_run_all_pipeline_in_one_script.sh

--- 00_scripts/000_run_all_pipeline_in_one_script.sh
+++ 00_scripts/000_run_all_pipeline_in_one_script.sh
@@ -2,6 +2,8 @@
 
 # Run all scripts :
 
-time nohup bash 05_qiime2_import_PE.sh &> 05_qiime2_import_PE.out
-time nohup bash 06_qiime2_denoise_PE.sh &> 06_qiime2_denoise_PE.out
+#time nohup bash 05_qiime2_import_PE.sh &> 05_qiime2_import_PE.out
+#time nohup bash 06_qiime2_denoise_PE.sh &> 06_qiime2_denoise_PE.out
 time nohup bash 07_qiime2_tree_PE.sh &> 07_qiime2_tree_PE.out
+time nohup bash 08_qiime2_rarefaction_PE.sh &> 08_qiime2_rarefaction_PE.out
+
